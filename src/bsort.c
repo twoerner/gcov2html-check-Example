5c7854352a5b4a4e17822a1e7ef586772646e9f8
Trevor Woerner <twoerner@gmail.com>
2011-04-20 00:07:21 -0400

    Add bubble sort example.
    First commit of bubble sort example; simple sort with simple Makefile.
    This example does not (yet) tie into the autotools system.

---
+++ src/bsort.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2011  Trevor Woerner
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRSZ 32
+
+static void randomize (int arr[], unsigned sz);
+static unsigned bsort (int arr[], unsigned sz);
+static void print_array (int arr[], unsigned sz);
+
+int
+main (void)
+{
+	int arr[ARRSZ];
+	unsigned swaps;
+
+	randomize (arr, ARRSZ);
+
+	printf ("start: ");
+	print_array (arr, ARRSZ);
+
+	swaps = bsort (arr, ARRSZ);
+
+	printf ("  end: ");
+	print_array (arr, ARRSZ);
+
+	printf ("swaps: %u\n", swaps);
+
+	return 0;
+}
+
+static void
+randomize (int arr[], unsigned sz)
+{
+	time_t now;
+	unsigned i;
+
+	now = time (NULL);
+	srand ((unsigned int)now);
+
+	for (i=0; i<sz; ++i)
+		arr[i] = rand () % sz;
+}
+
+static unsigned
+bsort (int arr[], unsigned sz)
+{
+	unsigned i, j;
+	int tmp;
+	bool swaps;
+	unsigned swapCnt = 0;
+
+	for (i=(sz-1); i>0; --i) {
+		swaps = false;
+		for (j=0; j<i; ++j) {
+			if (arr[j] > arr[j+1]) {
+				swaps = true;
+				++swapCnt;
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+		if (!swaps)
+			break;
+	}
+
+	return swapCnt;
+}
+
+static void
+print_array (int arr[], unsigned sz)
+{
+	unsigned i;
+
+	printf ("(");
+	for (i=0; i<sz; ++i) {
+		printf ("%u", arr[i]);
+		if (i != (sz - 1))
+			printf (" ");
+	}
+	printf (")\n");
+}
